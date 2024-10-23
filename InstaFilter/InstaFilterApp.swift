048e8e14cf624cd501d3de75cd8aad7224c3d8a0
sxmtee <somtoo76@gmail.com>
2024-10-23 16:09:59 +0100

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by mac on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
